4d39a4feed857641ac7bd6958f3cc588347e5771
Jaakko Ahola <jaakko.ahola@fmi.fi>
2021-02-10 16:25:00 +0200

    modified:   phase02_emulate_puhti.py 			- execute mode of the file added 	new file:   phase02_emulate_puhti_lvl3day.py 	new file:   phase02_emulate_puhti_lvl3night.py 	new file:   phase02_emulate_puhti_lvl4day.py 	new file:   phase02_emulate_puhti_lvl4night.py

--- phase02_emulate_puhti_lvl3day.py
+++ phase02_emulate_puhti_lvl3day.py
@@ -21,22 +21,7 @@ def main(inputConfigFile):
                                              [rootFolderOfEmulatorSets,  "case_emulator_DESIGN_v3.0.0_LES_ECLAIR_branch_ECLAIRv2.0.cray.fast_LVL3_night"],
                                              [rootFolderOfDataOutputs],
                                              inputConfigFile
-                                             ),
-                # "LVL3Day"   :  EmulatorData( "LVL3Day",
-                #                             [rootFolderOfEmulatorSets, "case_emulator_DESIGN_v3.1.0_LES_ECLAIR_branch_ECLAIRv2.0.cray.fast_LVL3_day"],
-                #                             [rootFolderOfDataOutputs],
-                #                             inputConfigFile
-                #                             ),
-                # "LVL4Night" :  EmulatorData("LVL4Night",
-                #                             [rootFolderOfEmulatorSets, "case_emulator_DESIGN_v3.2_LES_ECLAIR_branch_ECLAIRv2.0.cray.fast_LVL4_night"],
-                #                             [rootFolderOfDataOutputs],
-                #                             inputConfigFile
-                #                             ),
-                # "LVL4Day"   : EmulatorData("LVL4Day",
-                #                             [rootFolderOfEmulatorSets, "case_emulator_DESIGN_v3.3_LES_ECLAIR_branch_ECLAIRv2.0.cray.fast_LVL4_day"],
-                #                             [rootFolderOfDataOutputs],
-                #                             inputConfigFile
-                #                             )
+                                             )
                 }
 
     for key in emulatorSets:
@@ -52,6 +37,6 @@ if __name__ == "__main__":
     print("inputConfigFile", inputConfigFile)
 
     main(inputConfigFile)
-    
+
     end = time.time()
     print(f"\nScript completed in { end - start : .1f} seconds")
